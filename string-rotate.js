2fc34c5992347090f5da5e524ff8b2a9486ad5b7
yorch Adalbert <george_nlc@hotmail.com>
2023-04-12 17:44:07 -0500

    Rotate word challange v1.0.0

---
+++ string-rotate.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const rotate = str => {
+    const rotatedWordArr = []
+    let currentWord = str
+
+    for (let i = 1; i < str.length; i++) {
+        let lastLetter = ''
+        let rotatedWord = ''
+        
+        for (let j = 0; j < currentWord.length; j++) {
+            if (!j) {
+                lastLetter += currentWord[j]
+                continue
+            }
+            rotatedWord += currentWord[j]
+        }
+
+        rotatedWord += lastLetter
+        rotatedWordArr.push(rotatedWord)
+        currentWord = rotatedWord
+    }
+
+    rotatedWordArr.push(str)
+    
+    return rotatedWordArr
+}
+
+const str = 'Hello'
+
+console.log(rotate(str))
